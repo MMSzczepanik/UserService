356c2191a8b35a43b8b30f7b22b47104bbdda376
Michal Szczepanik <micszczepanik@gmail.com>
2019-01-26 13:08:24 +0100

    New Version

--- src/main/java/SDA/App.java
+++ src/main/java/SDA/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UserService userService = new UserService();
+        userService.add(new User("Michał", "Szczepanik", "mic@gmail.com", "XXX"));
+        userService.add(new User("Michał", "Ziółkowski", "micc@gmail.com", "XXY"));
+        userService.getUsers();
     }
 }
